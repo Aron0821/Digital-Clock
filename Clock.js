a7127770bb07a1b7f10a97d8f76e69b49b388ee6
Aron0821 <aronshakha101@gmail.com>
2024-04-08 12:15:20 +0545

    Complete.

---
+++ Clock.js
@@ -0,0 +1,14 @@
+let hrs = document.querySelector("#hrs");
+let min = document.querySelector("#min");
+let sec = document.querySelector("#sec");
+
+setInterval(() => {
+  let currentTIme = new Date();
+
+  hrs.innerHTML =
+    (currentTIme.getHours() < 10 ? "0" : "") + currentTIme.getHours();
+  min.innerHTML =
+    (currentTIme.getMinutes() < 10 ? "0" : "") + currentTIme.getMinutes();
+  sec.innerHTML =
+    (currentTIme.getSeconds() < 10 ? "0" : "") + currentTIme.getSeconds();
+}, 1000);
